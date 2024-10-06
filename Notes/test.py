30b48b8a25f5b911e2d4d1fdef7871a535954a62
Jolomi Taiwo <taiwojolomi15@gmail.com>
2024-10-06 23:52:30 +0100

    modified:   Day3 - IF-Elif-Else-and-Modules-in-Python.ipynb

--- Notes/test.py
+++ Notes/test.py
@@ -1,2 +1,33 @@
 ["Shade", 10, 45.9, True, ["Shade", 10, 45.9, True]]
+
 print(["Shade", 10, 45.9, True, ["Shade", 10, 45.9, True]])
+
+
+
+
+def ring1():
+
+   def ring2():
+
+      def ring3():
+
+         print(3)
+
+      def ring3_1():
+
+         print(3)
+
+   print(2)
+
+   def ring2_1():
+
+      def ring3():
+
+         print(3)
+
+      def ring3_1():
+
+         print(3)
+
+
+print('outer')
